9b323f7e812d3dbfbc82b892ef0ba40dffa90e7e
Ikhsan Lintang Prakasa <ikhsanlintang06@gmail.com>
2024-10-31 13:02:59 +0700

    popup Box

---
+++ script.js
@@ -0,0 +1,25 @@
+// PopUP Box
+// alert("Hello Wolrd");
+// alert("Ikhsan");
+// alert("Lintang");
+
+// const nama = prompt("masukan nama anda: ");
+
+// alert(`Halo ${nama}`);
+// console.log(nama);
+
+// const result = confirm("kamu yakin?");
+// if (result === true) {
+//   alert("ok");
+// } else {
+//   alert("no");
+// }
+alert("Selamat Datang Di Website Saya");
+var lagi = true;
+while (lagi) {
+  var nama = prompt("Siapa nama anda?");
+  alert(`Halo ${nama}`);
+  lagi = confirm("lagi?");
+}
+
+alert("Terima kasih");
